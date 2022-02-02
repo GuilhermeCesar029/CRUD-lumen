e7efc9c325bef5cc2351f63a8f1424a270c1717f
GuilhermeCesar029 <guicesar029@gmail.com>
2022-02-02 13:55:03 -0300

    Crud de Usuários

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function __contruct()
+    {
+
+    }
+
+    public function index()
+    {
+        try {
+
+            $users = User::all();
+            return response()->json($users);
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+
+    }
+
+    public function create(Request $request)
+    {
+        try {
+
+            $user = new User;
+
+            $user->use_name = $request->name;
+            $user->use_email = $request->email;
+            if (!empty($request->password)) {
+                $user->use_password = Hash::make($request->password);
+            }
+
+            $user->save();
+
+            return response()->json('User created successfully!', 201);
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+
+    }
+
+    public function show($id)
+    {
+        try {
+
+            $user = User::find($id);
+            if (!$user) {
+                return response()->json('User not found!', 404);
+            }
+
+            return response()->json($user);
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+
+            $user = User::find($id);
+
+            $user->use_name = $request->name;
+            $user->use_email = $request->email;
+            if (!empty($request->password)) {
+                $user->use_password = Hash::make($request->password);
+            }
+
+            $user->save();
+
+            return response()->json('User successfully updated!');
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+
+            $user = User::find($id);
+            if (!$user) {
+                return response()->json('User not found!', 404);
+            }
+
+            $user->delete();
+
+            return response()->json('User successfully removed!');
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+    }
+
+
+}
---
+++ api/database/migrations/2022_02_02_142153_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('use_id');
+            $table->string('use_name');
+            $table->string('use_email');
+            $table->string('use_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
